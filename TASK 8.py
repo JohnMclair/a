f545c14e308d6898ae1e8d04d5c22c879959dc97
AMIN <alamin_Salim@Hotmail.com>
2022-03-01 02:59:02 +0800

    Script of amin (Task8)

---
+++ TASK 8.py
@@ -0,0 +1,10 @@
+#This is to import the unittest program
+import unittest
+#THis is to test the following app specfically
+class scrappytest(unittest.TestCase):
+
+    def test_EngineType(self):
+        print("Testing")
+
+if __name__ == '__main__':
+    unittest.main()
